4ce978a38b9c9d5b8883537174bba6a05d6f1859
terka2610 <122984973+terka2610@users.noreply.github.com>
2024-07-11 14:43:02 +0200

    Create streamlit.py

---
+++ streamlit.py
@@ -0,0 +1,6 @@
+import pandas as pd 
+import plotly.express as px 
+import streamlit as stimport pandas as pd 
+import plotly.express as px 
+import streamlit as st
+st.set_page_config(page_title="AgriculturalExports",page_icon=":seedling",layout="wide")
